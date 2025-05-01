3aabe3190a49c4518cc3afe7d05d2cf68d4d3221
Roman <rzozulya1997@gmail.com>
2025-05-01 10:21:27 +0300

    init

---
+++ src/test/java/com/example/Testing/System/TestingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Testing.System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
