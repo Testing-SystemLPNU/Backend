186580f7c56e4b5bf51d87bce5e1da2a55c545f1
Roman <rzozulya1997@gmail.com>
2025-05-05 22:06:36 +0300

    minor fix

---
+++ src/main/java/com/example/Testing/System/config/CorsConfig.java
@@ -0,0 +1,25 @@
+package com.example.Testing.System.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // або обмежено на /auth/**
+                        .allowedOrigins("*") // або заміни * на конкретне походження, наприклад "https://swagger-ui.com"
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
